2daeca9e27b07940e7e5e07a6ac6c8b9a492328d
Ivo Domingos <2229639d@student.gla.ac.uk>
2018-06-10 21:26:19 +0100

    Final Version
    API Routes added to README.md
    deleted unnecessary route

--- simpleLogin/routes/users.js
+++ simpleLogin/routes/users.js
@@ -7,11 +7,6 @@ var passport = require('passport');
 var User = require('../models/user');
 var localStrategy = require('passport-local').Strategy;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.get('/register', function(req, res, next) {
   res.render('register', {title: 'Register'});
 });
